cdeb5c1fd4eece7f08f7caca803cdd5733bf58cf
mariapesm <youandmecanyousee@hotmail.com>
2018-11-26 22:38:14 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -20,11 +20,32 @@ module.exports = function (app) {
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      if (initNum == "invalid number" && initUnit == "invalid unit")
+      {
+        res.send('invalid number and unit');
+      }
+      else if (initNum == "invalid number")
+      {
+        res.send('invalid number');
+      }
+      else if (initUnit == 'invalid unit')
+      {
+        res.send('invalid unit');
+      }
+      else
+      {
+        var returnNum = convertHandler.convert(initNum, initUnit);
+        var returnUnit = convertHandler.getReturnUnit(initUnit);
+        var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       
-      //res.json
+        res.json({
+          'initNum': initNum,
+          'initUnit': initUnit,
+          'returnNum': returnNum,
+          'returnUnit': returnUnit,
+          'string': toString
+        });
+      }
     });
     
 };
